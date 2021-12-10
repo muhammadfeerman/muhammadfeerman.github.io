4df599a5d47139d16343ad87bc85bd3fb88c40b0
muhammadfeerman <93190191+muhammadfeerman@users.noreply.github.com>
2021-12-10 21:58:54 +0800

    Seperate folder

---
+++ Assignment 2/feedback.js
@@ -0,0 +1,62 @@
+const form = document.getElementById('form');
+const username = document.getElementById('name');
+const email = document.getElementById('email');
+const subject = document.getElementById('subject');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const subjectValue = subject.value.trim();
+
+    if(usernameValue == '') {
+        setError(username, 'Name is required');
+        window.alert("sometext");
+    } else {
+        setSuccess(username);
+    }
+
+    if(emailValue == '') {
+        setError(email, 'Email is required');
+        window.alert("sometext");
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address');
+    } else {
+        setSuccess(email);
+    }
+
+    if(subjectValue == '') {
+        setError(subject, 'Message is required');
+        window.alert("sometext");
+    } else {
+        setSuccess(subject);
+    }
+}
